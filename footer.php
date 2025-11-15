b10fd4ce00b0fd4e31310fcd62e3bf7ff34c484a
wien92Coder <winarno92@gmail.com>
2025-11-15 18:36:10 +0700

    Adding get_header and get_footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Footer template.
+ * 
+ * @package Equila
+ */
+?>
+
+<footer>Footer</footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,16 +5,13 @@
  * @package Equila
  */
 
+get_header();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WordPress Theme</title>
-</head>
-<body>
-    <h1>Hello WordPress</h1>
-</body>
-</html>
+
+<content class="content">Content</content>
+
+<?php
+
+get_footer();
+    
